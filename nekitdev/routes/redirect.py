c991db0751ca60e05c062e604122930842721682
Nikita Tikhonov <nekitdevofficial@gmail.com>
2022-05-19 15:21:54 +0300

    Add Keybase redirect.

--- nekitdev/routes/redirect.py
+++ nekitdev/routes/redirect.py
@@ -5,11 +5,13 @@ DISCORD = "discord"
 DISCORD_LINK = "https://discord.gg/KtJkbut"
 
 GITHUB = "github"
+KEYBASE = "keybase"
 PATREON = "patreon"
 TWITTER = "twitter"
 YOUTUBE = "youtube"
 
 GITHUB_LINK = "https://github.com/nekitdev"
+KEYBASE_LINK = "https://keybase.io/nekitdev"
 PATREON_LINK = "https://patreon.com/nekitdev"
 TWITTER_LINK = "https://twitter.com/nekitdev"
 YOUTUBE_LINK = "https://youtube.com/nekitdev"
@@ -17,6 +19,7 @@ YOUTUBE_LINK = "https://youtube.com/nekitdev"
 NAME_TO_LINK = {
     DISCORD: DISCORD_LINK,
     GITHUB: GITHUB_LINK,
+    KEYBASE: KEYBASE_LINK,
     PATREON: PATREON_LINK,
     TWITTER: TWITTER_LINK,
     YOUTUBE: YOUTUBE_LINK,
