e35a6fa4a4b8e4197871eee0d0c5719256a4c403
Nikita Tikhonov <nekitdevofficial@gmail.com>
2022-06-29 09:06:59 +0300

    Add webmanifest and metadata.

--- nekitdev/__init__.py
+++ nekitdev/__init__.py
@@ -1,2 +1,14 @@
+"""nekit.dev - Web application written in Python."""
+
+__description__ = "nekit.dev - Web application written in Python."
+__url__ = "https://github.com/nekitdev/nekit.dev"
+
+__title__ = "nekitdev"
+__author__ = "nekitdev"
+__license__ = "MIT"
+__version__ = "1.0.0-alpha.1"
+
 from nekitdev import modules
 from nekitdev.core import setup_app
+
+__all__ = ("modules", "setup_app")
