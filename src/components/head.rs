8eb5e2eb73f8bb2e5c055fef15a6bf6b0aba9339
Nikita Tikhonov <nekitdevofficial@gmail.com>
2025-11-22 19:35:18 +0300

    Use `icon` names for icons.

--- src/components/head.rs
+++ src/components/head.rs
@@ -6,8 +6,8 @@ pub const STYLE: Asset = asset!(
     "/assets/tailwind.css",
     CssAssetOptions::new().with_preload(true)
 );
-pub const IMAGE_PNG: Asset = asset!("/assets/images/icon.png");
-pub const IMAGE_SVG: Asset = asset!("/assets/images/icon.svg");
+pub const ICON_PNG: Asset = asset!("/assets/images/icon.png");
+pub const ICON_SVG: Asset = asset!("/assets/images/icon.svg");
 
 pub const ICONS: &str = "https://kit.fontawesome.com/c7493dda5d.js";
 
@@ -17,7 +17,7 @@ pub const TYPE: &str = "website";
 
 #[component]
 pub fn Head(title: String, description: String, url: String) -> Element {
-    let image = urls::asset(IMAGE_PNG);
+    let icon = urls::asset(ICON_PNG);
 
     rsx! {
         document::Meta { name: "viewport", content: "width=device-width, initial-scale=1" }
@@ -27,7 +27,7 @@ pub fn Head(title: String, description: String, url: String) -> Element {
         document::Meta { property: "og:title", content: title.clone() }
         document::Meta { property: "og:type", content: TYPE }
         document::Meta { property: "og:url", content: url.clone() }
-        document::Meta { property: "og:image", content: image }
+        document::Meta { property: "og:image", content: icon }
         document::Meta { property: "og:description", content: description.clone() }
         document::Meta { property: "og:site_name", content: NAME }
         document::Meta { property: "og:locale", content: LANG }
@@ -40,9 +40,9 @@ pub fn Head(title: String, description: String, url: String) -> Element {
 
         document::Stylesheet { href: STYLE }
 
-        document::Link { rel: "icon", href: IMAGE_PNG }
-        document::Link { rel: "icon", href: IMAGE_SVG }
-        document::Link { rel: "apple-touch-icon", href: IMAGE_PNG }
+        document::Link { rel: "icon", href: ICON_PNG }
+        document::Link { rel: "icon", href: ICON_SVG }
+        document::Link { rel: "apple-touch-icon", href: ICON_PNG }
 
         document::Script {
             async: "true",
