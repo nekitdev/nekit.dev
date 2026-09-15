a8988c68369bdd097cbc567b2a1f2be3fc3a5a96
Nikita Tikhonov <nekit@nekit.dev>
2026-09-15 22:11:32 +0300

    Use `pid1` for correct shutdown in `docker`, add multi-arch `build`.

--- crates/nekit-dev/src/main.rs
+++ crates/nekit-dev/src/main.rs
@@ -1,5 +1,8 @@
 use nekit_dev::routes::App;
 
 fn main() {
+    #[cfg(feature = "server")]
+    pid1::relaunch_if_pid1().expect("failed to relaunch");
+
     dioxus::launch(App);
 }
--- crates/nekit-serve/src/main.rs
+++ crates/nekit-serve/src/main.rs
@@ -1,8 +1,11 @@
 use anyhow::Result;
 use clap::Parser;
 use nekit_serve::app::App;
+use pid1::relaunch_if_pid1;
 
 fn main() -> Result<()> {
+    relaunch_if_pid1().expect("failed to relaunch");
+
     App::parse().run()?;
 
     Ok(())
